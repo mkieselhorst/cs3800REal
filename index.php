d110a4b3cdd78629462e1cf8018da7a31ef458f5
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2024-01-09 18:25:22 -0500

    ind

--- index.php
+++ index.php
@@ -6,6 +6,25 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <!-- commenting in html --> 
+    <h1>this is my first php  say hello</h1>
+
+    <?php
+        //  XAMPP stands for : Cross-platform Apache MySQL php perl
+        // commenting in php
+        /* also commenting*/
+        # also commenting
+        /*
+            Variables in php declared by starting with $
+            php is loose language 
+         */
+        $name = "Max ";
+        echo $name;
+        echo "<br />";
+        $quantity = 10;
+        $name = 65;
+        echo $name;
+
+    ?>
 </body>
 </html>
