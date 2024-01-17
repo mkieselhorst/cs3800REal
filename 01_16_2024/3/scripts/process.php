1b03b0a9d182fb3c2cb7b0e15cfe3da840e9b712
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2024-01-16 19:00:02 -0500

    finished

--- 01_16_2024/3/scripts/process.php
+++ 01_16_2024/3/scripts/process.php
@@ -5,11 +5,16 @@
         $phone      = filter_input(INPUT_POST, 'phone');
         $email      = filter_input(INPUT_POST, 'email');
         $document   = filter_input(INPUT_POST, 'document');
+        // Handle checking, for 0 choice, 1, 2, or all
+        $services   = $_POST['services'] ?? [];
+
         $phone      = preg_replace('/[0-9]/', '', $phone);
 
             echo("Name: " .htmlspecialchars($name) . "<br/>" .
             "Phone: " .htmlspecialchars($phone) . "<br/>" .
             "Email: " . htmlspecialchars($email) . "<br/>" .
-            "Document: " . htmlspecialchars($document) . "<br/>");
+            "Document: " . htmlspecialchars($document) . "<br/>" .
+            "Services: " . htmlspecialchars(implode(", " , $services)) . "<br/>" 
+    );
     }
 ?>
