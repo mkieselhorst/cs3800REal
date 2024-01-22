5fdd5a64f7c54fc5ae772be6e08bf15dceffc265
mkieselhorst <kieselhorst.3@wright.edu>
2024-01-22 16:37:08 -0500

    php done

--- Assignments/Student_Registration_Form/Scripts/process.php
+++ Assignments/Student_Registration_Form/Scripts/process.php
@@ -1,19 +1,21 @@
 <?php
     if($_SERVER['REQUEST_METHOD'] == "POST" ){
 
-        $name       = filter_input(INPUT_POST, 'name');
-        $email      = filter_input(INPUT_POST, 'email');
+        $name       = filter_input(INPUT_POST, 'name', );
+        $email      = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $phone      = filter_input(INPUT_POST, 'phone');
         $status   = filter_input(INPUT_POST, 'status');
         // Handle checking, for 0 choice, 1, 2, or all
-        $interests   = $_POST['interest'] ?? [];
+        $interests   = $_POST['interests'] ?? [];
+        $major   = $_POST['major'] ?? [];
 
-        $phone      = preg_replace('/[0-9]/', '', $phone);
+        $phone      = preg_replace('/[^0-9]/', '', $_POST['phone']);
 
             echo("Name: " . htmlspecialchars($name) . "<br/>" .
             "Email: " . htmlspecialchars($email) . "<br/>" .
             "Phone: " . htmlspecialchars($phone) . "<br/>" .
             "Status: " . htmlspecialchars($status) . "<br/>" .
-            "Interests: " . htmlspecialchars(implode(", " , $interest)) . "<br/>" 
+            "Interests: " . htmlspecialchars(implode(', ', $interests)) . "<br/>" . 
+            "Majors: " . htmlspecialchars(implode(', ', $major)) . "<br/>"
     );
     }
