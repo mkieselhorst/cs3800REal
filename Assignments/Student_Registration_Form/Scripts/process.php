b9c172ba88c4b5a02fff16b00a968e1f5e8f6f07
mkieselhorst <kieselhorst.3@wright.edu>
2024-01-22 17:05:06 -0500

    comments

--- Assignments/Student_Registration_Form/Scripts/process.php
+++ Assignments/Student_Registration_Form/Scripts/process.php
@@ -1,16 +1,21 @@
 <?php
+// request
     if($_SERVER['REQUEST_METHOD'] == "POST" ){
 
+// get all input values as vars
         $name       = filter_input(INPUT_POST, 'name', );
         $email      = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
         $phone      = filter_input(INPUT_POST, 'phone');
         $status   = filter_input(INPUT_POST, 'status');
-        // Handle checking, for 0 choice, 1, 2, or all
+
+// get arrays for interests and majors
         $interests   = $_POST['interests'] ?? [];
         $major   = $_POST['major'] ?? [];
 
+// phone number sanitization
         $phone      = preg_replace('/[^0-9]/', '', $_POST['phone']);
 
+// echo all values necessary
             echo("Name: " . htmlspecialchars($name) . "<br/>" .
             "Email: " . htmlspecialchars($email) . "<br/>" .
             "Phone: " . htmlspecialchars($phone) . "<br/>" .
