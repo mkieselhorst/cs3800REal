ced2c86ef2114c291248c45c4baea7d832f3739e
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2024-03-05 18:04:02 -0500

    update facultyTable.php

--- classwork/03_05_2024-mvc/facultyTable.php
+++ classwork/03_05_2024-mvc/facultyTable.php
@@ -1,14 +1,29 @@
 <?php
-require_once'facultyDDatabase.php';
+require_once'facultyDatabase.php';
 class FacultyTable{
     private $dbConnection;
+
+
+    // main constructor to establish connection to the db and server
     public function __construct($dbConnection){
         $this->dbConnection = $dbConnection->conn;
         $this->createTable();
     }
 
-    
+    private function createTable(){
+        
+        
+        $sql = "CREATE TABLE IF NOT EXISTS professors (
+                ID INT AUTO_INCREMENT PRIMARY KEY, 
+                NAME VARCHAR(255) NOT NULL, 
+                DEPARTMENT VARCHAR(255) NOT NULL)";
 
+        
+    
+            if(!$this->dbConnection->query($sql)){
+                echo "Error: creating table FacultyTable failed" . $this->dbConnection->error;
+            }   
+    }
 }
 
 
