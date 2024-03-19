53d1fd955a009914da66cfcff1a06a55acc5050e
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2024-03-19 17:02:29 -0400

    create 1 file and update 3 files

--- classwork/03_12_2024/Scripts/connection.php
+++ classwork/03_12_2024/Scripts/connection.php
@@ -1,4 +1,30 @@
 <?php
+    $servername='localhost';
+    $username='root';
+    $password='';
+    $database='maxwellk';
+    // create connection with mysqli_connect method
+    $connect = mysqli_connect($servername, $username, $password, $database);
 
+    if(!$connect){
+        echo("connection failed" . mysqli_connect_error());
+    }
+    else{
+        echo("connected successfully" . $database);
+    }
+// sql create table
+    $sql = "CREATE TABLE IF NOT EXISTs TBLSTUDENT(
+        studentId INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+        studentFirstName VARCHAR(50) NOT NULL, 
+        studentLastName VARCHAR(50) NOT NULL, 
+        studentMajor VARCHAR(25) NOT NULL, 
+        REG_DATE TIMESTAMP)";
+    // CREATE WITH BUILT IN  FUNCTION
+    // $connect and $sql
+    if(mysqli_query($connect, $sql)){
+        echo("tblStudent created or already eists");
+    }else{
+        echo("Error, could not create or find table");
+    };
 
 ?>
