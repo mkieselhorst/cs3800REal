daecc212b12ad10df284e93fceb56bbb1bd81cfc
mkieselhorst <89440801+mkieselhorst@users.noreply.github.com>
2024-04-09 18:16:52 -0400

    fixed it

--- classwork/04_09_2024/scripts/connection.php
+++ classwork/04_09_2024/scripts/connection.php
@@ -1,31 +1,36 @@
-<?
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "csDept";
+<?php
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+$servername     = "localhost";
+$username       = "root";
+$password       = "";
+$dbname         = "csdept";
 // create connection
-$conn       = mysqli_connect($servername, $username, $password);
+$conn           = mysqli_connect($servername, $username, $password);
 
 // Check connections
 if(!$conn){
     die("could not connect to db" . mysqli_connect_error());
+}else{
+    echo("connection established");
 }
 
 
 // create db
-$sql        = "CREATE DATABASE IF NOT EXISTS $dbname ";
-$name = "max";
-echo $name;
-// run the creation of db
-if(!mysqli_query($conn, $sql)){
-    echo("Error processing the query to creat the database: " . mysqli_error($conn));
+$sql            = "CREATE DATABASE IF NOT EXISTS $dbname";
+if (mysqli_query($conn, $sql)) {
+    echo "Database created successfully";
+} else {
+    echo "Error creating database: " . mysqli_error($conn);
 }
+
 
 // select the database
 mysqli_select_db($conn, $dbname);
 
 // create the table
-$sql        = "CREATE TABLE IF NOT EXISTS tblStudents 
+$sql            = "CREATE TABLE IF NOT EXISTS tblStudents
                 ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
                 firstName   VARCHAR(30) NOT NULL,
                 lastName    VARCHAR(30) NOT NULL,
--- classwork/04_09_2024/scripts/insert.php
+++ classwork/04_09_2024/scripts/insert.php
@@ -1,4 +1,4 @@
-<?
+<?php
 // include connection.php in useage
 include "../scripts/connection.php";
 
@@ -11,5 +11,23 @@ $imageFIleType  = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 // attempt to upload
 if(move_uploaded_file($_FILES['image']["tmp_name"], $target_file)){
     echo("The file " . htmlspecialchars(basename($_FILES['image']["name"])) . "has been uploaded");
+
+    $firstName  = $_POST['firstName'];
+    $lastName   = $_POST['lastName'];
+    $email      = $_POST['email'];
+    $city       = $_POST['city'];
+    $state      = $_POST['state'];
+    $zipCode    = $_POST['zipCode'];
+    $imagePath  = $target_file;
+
+    $sql        = "INSERT INTO tblStudents(firstName, lastName, email, city, state, zipCode, imagePath)
+                            VALUES('$firstName, $lastName, $email, $city, $state, $zipCode, $imagePath')";
+
+
+    if(!mysqli_query($conn, $sql)){
+        echo("Error inserting values into TABLE: " . mysqli_error($conn));
+    }else{
+        echo("New record created");
+    }
 }
 ?>
